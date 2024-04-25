ae4fa0077e3528026aa7ddbeabdb5d1349c25ac5
Shiv Shah <shivshahla@gmail.com>
2024-04-25 15:09:38 -0700

    pls be good

--- src/pages/about/aboutcard.tsx
+++ src/pages/about/aboutcard.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
+
 
 const AboutCard = () => {
   return (
     // <div className="max-w-md mx-auto bg-ab-alt-bg rounded-lg shadow-md overflow-hidden md:amx-w-4xl flex justify-between p-4">
     <div className="max-w-xl mx-auto bg-ab-alt-bg rounded-lg shadow-md overflow-visible md:max-w-4xl flex flex-col p-6 relative">
-      <div className="absolute -right-16 top-0 z-0">
+      <div className="hidden sm:block  absolute -right-16 top-0 z-0">
         <Image
           src="/abbychina.jpeg"
           alt="Oval Image"
@@ -16,7 +16,7 @@ const AboutCard = () => {
           height={600}
         />
       </div>
-      <div className="absolute -right-10 top-40 z-10">
+      <div className="hidden sm:block absolute -right-10 top-40 z-10">
         <Image
           src="/abbyseekout.jpeg"
           alt="Rectangle Image"
@@ -26,7 +26,7 @@ const AboutCard = () => {
         />
       </div>
       <div>
-        <div className="pr-28">
+        <div className="md:pr-28">
           <div className="text-5xl uppercase font-tenor">Who is Abigayle?</div>
           <div className="flex items-center mt-2">
             <FontAwesomeIcon
@@ -44,40 +44,22 @@ const AboutCard = () => {
             that uses a community-powered approach to connect youth to mental
             health support. She started Magnify Wellness as an iOS app and then
             expanded the passion project into a full-fledged international
-            registered nonprofit organization. 
-            Since 2020, Abigayle led the 80+ team to grow Magnify Wellness to <span className="font-bold">100+</span> chapter clubs worldwide,
-            <span className="font-bold"> 2k+</span> Instagram followers, and
-            <span className="font-bold"> 4k+</span> App Store downloads.
+            organization.
           </p>
           <p className="mt-2 leading-relaxed font-nunito">
-            Eventually, Magnify Wellness won the title of &apos;Top
+            Abigayle led the 80+ team of volunteers to build out Magnify
+            Wellness&apos;s portfolio of initiatives, winning the title of &apos;Top
             Global Health Innovations&apos; from the Bay Area Global Health
             competition as a college freshman with contenders from Stanford
-            University&apos; School of Medicine, Yale School of Medicine, and more. Abigayle loves to mentor other people, and has
+            Medicine and more. Abigayle loves to mentor others, and has
             consulted hundreds of students and professionals on their passion
-            projects and career, leading her to build a highly engaged 12k+
-            audience across LinkedIn, Instagram, and her career
-            newsletter known as Supercharged on Substack. Abigayle’s work experience positions her as a{" "}
+            projects and career, leading her to build a highly engaged 11k+
+            followers across LinkedIn and Instagram, and her Substack
+            newsletter. Abigayle’s work experience positions her as a{" "}
             <span className="font-bold">unique</span> and{" "}
             <span className="font-bold">invaluable</span> creative partner in
-           authentically building your brand&apos; voice and community.
+            cultivating an engaged community for your next ad campaign.
           </p>
-        </div>
-         {/* Text "Sound like a good fit?" */}
-        <div className="flex justify-center items-center mt-6">
-          <p className="text-xl md:text-2xl font-nunito mr-4">
-            Sound like a good fit? 
-            Email me abigayleugc@gmail.com
-          </p>
-          {/* "Work With Me" button */}
-        
-          <Link
-              href={"/#contact"}
-             className="bg-black text-white rounded-md px-8 py-4 hover:bg-gray-800 transition-colors font-nunito text-xl md:text-2xl"
-            >
-              Book Me
-            </Link>
-
         </div>
       </div>
     </div>
