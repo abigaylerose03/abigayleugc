b0ecdc2350050781de6543fa2645f59ac0920e52
Abigayle Peterson <abigaylerose03@gmail.com>
2024-05-01 16:32:06 -0700

    Update services.tsx

--- src/pages/components/services.tsx
+++ src/pages/components/services.tsx
@@ -15,38 +15,48 @@ export default function Services() {
 
         <div className="flex flex-col items-center text-center">
           <h2 className="text-2xl font-bold underline font-unbounded">
-           🔎 UGC Paid Ads and UGC Organic
+            🔎 UGC Paid Ads and UGC Organic
           </h2>
           <div className="text-lg font-nunito">
             <p className="my-2">
               I create ads that convert with an eye for aesthetics.
             </p>
-            <p className="my-2">This Includes:</p>
-            <ul className="list-disc pl-4">
-              <li>Concept ideation process</li>
-              <li>Script writing</li>
-              <li>Direct-response marketing: video with hook, body, and CTA</li>
-              <li>Revisions (if any)</li>
-              <li>Add-ons (Paid ads): Paid ad usage rights raw footage, multiple hooks, rush fee (1-3 days), monthly retainers</li>
-              <li>Add-ons (Organic content): Monthly retainers, raw footage, UGC photos, product photography, rush fee (1-3 days), re-films, exclusitivity</li>
-            </ul>
+            <div className="my-2">
+              <details>
+                <summary className="font-bold mb-2">This Includes</summary>
+                <ul className="pl-4">
+                  <li>Concept ideation process</li>
+                  <hr className="my-1 border-gray-300" />
+                  <li>Script writing</li>
+                  <hr className="my-1 border-gray-300" />
+                  <li>Direct-response marketing: video with hook, body, and CTA</li>
+                  <hr className="my-1 border-gray-300" />
+                  <li>Revisions (if any)</li>
+                  <hr className="my-1 border-gray-300" />
+                  <li>Add-ons (Paid ads): Paid ad usage rights raw footage, multiple hooks, rush fee (1-3 days), monthly retainers</li>
+                  <hr className="my-1 border-gray-300" />
+                  <li>Add-ons (Organic content): Monthly retainers, raw footage, UGC photos, product photography, rush fee (1-3 days), re-films, exclusitivity</li>
+                </ul>
+              </details>
+            </div>
           </div>
         </div>
         <div className="flex flex-col items-center text-center">
           <h2 className="text-2xl font-bold underline font-unbounded">
-           🔎 UGC Photography
+            🔎 UGC Photography
           </h2>
-          <div className="text-xl">
-            <p className="my-4 font-nunito">
-              I create aesthetic photos with a brand&apos;s voice as top of mind
-            </p>
-            <p className="mb-2 font-nunito">This Includes:</p>
-            <ul className="list-disc pl-4">
-              <li>Ideation process</li>
-              <li>Raw images</li>
-              <li>Revisions and editing</li>
-            </ul>
-          </div>
+          <div className="my-2">
+              <details>
+                <summary className="font-bold mb-2">This Includes</summary>
+                <ul className="pl-4">
+                  <li>Ideation process</li>
+                  <hr className="my-1 border-gray-300" />
+                  <li>Raw images</li>
+                  <hr className="my-1 border-gray-300" />
+                  <li>Revisions and editing</li>
+                </ul>
+              </details>
+            </div>
         </div>
       </div>
     </main>
